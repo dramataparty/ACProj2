56a83e2c708e17c19bcdf7bf6dda8680b6fff2a8
Dramata <diogobailaoforte@gmail.com>
2023-03-26 20:37:59 +0100

    svv

--- ticker_server.py
+++ ticker_server.py
@@ -26,21 +26,21 @@ class resource:
         self.resource_id=resource_id
         pass # Remover esta linha e fazer implementação da função
 
-        def subscribe(self, client_id, time_limit):
-            return self.subscribe(self, client_id, time_limit)
+    def subscribe(self, client_id, time_limit):
+        return self.subscribe(self, client_id, time_limit)
 
-        def unsubscribe (self, client_id):
-            return resource_pool.unsubscribe(self, client_id)
+    def unsubscribe (self, client_id):
+        return resource_pool.unsubscribe(self, client_id)
 
-        def status(self, client_id):
-            return resource_pool.status(self, client_id)
+    def status(self, client_id):
+        return resource_pool.status(self, client_id)
 
-        def __repr__(self):
-            output = ""
-            
-            # R <resource_id> <list of subscribers>
+    def __repr__(self):
+        output = ""
+        
+        # R <resource_id> <list of subscribers>
 
-            return output
+        return output
 
 
 
@@ -50,40 +50,38 @@ class resource_pool:
         self.N = N
         self.K = K
         self.M = M
-    pass
+        self.N, self.K, self.M = sel.select(SocketList, [], [])
+             
+        sock = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+        SocketList = [sock]
+        while True:
+            R, W, X = sel.select(SocketList, [], [])
+            for s in R:
+                if s is sock: 
+                    conn_sock, (addr, port) = sock.accept()
+                    SocketList.append(conn_sock)
+                else: 
+                    s.close() 
+                    SocketList.remove(s)
+                    print('Cliente fechou ligação')
+            pass
 
     global msg
-    
-    
 
-SocketList = [ListenSocket]
-while True:
-    R, W, X = sel.select(SocketList, [], []) # Espera sockets com
-    for sckt in R: # dados para leitura
-        if sckt is ListenSocket: # Se for a socket de escuta
-            conn_sock, (addr, port) = ListenSocket.accept()
-            print('Novo cliente ligado desde %s:%d' % (addr, port))
-        SocketList.append(conn_sock) # Adiciona ligação à lista
-        else: # Se for a socket de um cliente
-            msg = sckt.recv(1024)
-        if msg: # Se recebeu dados
-            sckt.sendall(msg[::-1]) # responde
-        else: # Se não recebeu dados
-            sckt.close() # cliente fechou ligação
-            SocketList.remove(sckt)
-            print('Cliente fechou ligação')
 
 
 # Remover esta linha e fazer implementação da função
 
     def clear_expired_subs(self):
-        delay = 10
+        delay = self.time_limit
         while True:
             time.sleep(delay)
             now = time.time()
             self.subs[:] = [elem for elem in self.subs if now - elem[1] < delay]
 
     def subscribe(self, resource_id, client_id, time_limit):
+        self.time_limit = time_limit
         if(client_id not in self.subs):
             self.subs.update({resource_id:client_id})
             return 'True'
